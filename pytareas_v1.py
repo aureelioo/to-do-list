5595eb06bd5ec84a29caddc6dba959cc232956b7
aureelioo <byisco29@gmail.com>
2025-10-22 15:45:44 -0400

    segundo commit: implementacion de POO (sin la opcion de marcar y desmarcar las tareas)

--- pytareas_v1.py
+++ pytareas_v1.py
@@ -1,29 +1,62 @@
 #PyTareas
 
-#lista de tareas 
-tareas = ["Preparar café", "Revisar el correo"]
 
-#funciones para ver y añadir tareas
-def mostrar_tareas(lista):
-    """Muestra todas las tareas con su índice."""
-    if not lista: #verifica si la lista está vacía
+#POO
+
+class Tarea:
+    """Clase para crear objetos Tarea"""
+
+    # El método constructor (__init__) se llama automáticamente cuando creamos un nuevo Objeto.
+    def __init__(self, nombre_de_tarea):
+
+        # 'self' se refiere al objeto que estamos creando (la galleta individual).
+        self.nombre = nombre_de_tarea  #Guarda el nombre
+        self.completada = False  #Inicializa el estado como Falso (no completada)
+
+    #Lo que la tarea puede hacer
+    def marcar_como_completa(self):
+        """Cambia el estado de la tarea a completada."""
+
+        self.completada = True
+        print(f"¡Tarea '{self.nombre}' marcada como completada! ✅")
+
+
+#lista para guardar objetos tarea
+tareas_objetos = []
+
+
+#funciones para ver 
+def mostrar_tareas(lista_objetos):
+    """Muestra todas las tareas y su estado (marcado con [X] o [ ])."""
+
+    if not lista_objetos: #verifica si la lista está vacía
         print("¡La lista está vacía!")
         return #salir de la funcion
     
     print("\n=============================")
     print("TU LISTA ACTUAL DE TAREAS:")
     print("=============================")
-    for indice, tarea in enumerate(lista):
+
+    for indice, tarea in enumerate(lista_objetos):
+
+        #ternario para estado de completado
+        estado = "[X]" if tarea.completada else "[ ]"
+
         #muestra el índice y la tarea
-        print(f" {indice + 1}. {tarea}")
+        print(f" {indice + 1}. {estado} {tarea.nombre}")
     print("=============================\n")
 
+
 #funcion de agregar tarea
-def agregar_tarea(lista):
-    """Pide una tarea y la añade a la lista."""
-    nueva_tarea = input("Escribe la nueva tarea: ")
-    lista.append(nueva_tarea) #append agrega al final de la lista
-    print(f"Tarea añadida: '{nueva_tarea}'")
+def agregar_tarea(lista_objetos):
+    """Pide una tarea, crea un objeto Tarea y lo añade a la lista."""
+    nueva_tarea_nombre = input("Escribe la nueva tarea: ")
+
+    #creamos el objeto tarea a partir de la clase
+    nuevo_objeto_tarea = Tarea(nueva_tarea_nombre)
+
+    lista_objetos.append(nuevo_objeto_tarea) #append agrega al final de la lista
+    print(f"Tarea añadida: '{nuevo_objeto_tarea.nombre}'")
 
 
 #funcion para eliminar tarea
@@ -77,17 +110,17 @@ while ejecutando:
     if opcion == "añadir":
         print("¡Perfecto! Vamos a agregar una nueva tarea.")
         # llamar a la función de agregar
-        agregar_tarea(tareas)
+        agregar_tarea(tareas_objetos)
 
     elif opcion == "ver":
         print("Aquí está tu lista de tareas.")
         # llamar a la función de ver
-        mostrar_tareas(tareas)
+        mostrar_tareas(tareas_objetos)
 
     elif opcion == "eliminar":
         print("Entendido, que tarea quieres eliminar?")
         # llamar a la función de eliminar
-        eliminar_tarea(tareas)
+        eliminar_tarea(tareas_objetos)
 
     elif opcion == "salir":
         print("Saliendo del programa PyTareas. ¡Adiós!")
